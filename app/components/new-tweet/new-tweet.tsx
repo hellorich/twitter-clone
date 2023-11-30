f01786810d2cba2f72153ed07d298fb4bf1f49cd
Rich Cookson <hellorich@frobitz.com>
2023-11-30 13:39:07 +0000

    Connected NewTweet to backend

--- app/components/new-tweet/new-tweet.tsx
+++ app/components/new-tweet/new-tweet.tsx
@@ -1,13 +1,23 @@
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
 
 export const NewTweet = () => {
-  const addTweet = async () => {
+  const addTweet = async (formData: FormData) => {
     'use server'
-    console.log('Submitted')
+    const title = String(formData.get('title'))
+    const supabase = createServerActionClient<Database>({ cookies })
+    const { 
+      data: { user }
+    } = await supabase.auth.getUser()
+
+    if (user) {
+      await supabase.from('tweets').insert({ title, user_id: user.id })
+    }
   }
 
   return (
     <form action={addTweet}>
-      <input name="title" />
+      <input name="title" className="bg-inherit" />
     </form>
   )
 }
