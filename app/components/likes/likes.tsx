e726f75cb5dba45d2977ffb55ab2a5b94a8ab1ed
Rich Cookson <hellorich@frobitz.com>
2023-12-04 10:50:34 +0000

    Fixed error with useOptimistic using startTransition - added dynamic tweet list updates

--- app/components/likes/likes.tsx
+++ app/components/likes/likes.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { startTransition } from 'react'
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useRouter } from "next/navigation"
 
@@ -8,6 +9,7 @@ export const Likes = ({ tweet, addOptimisticTweet } : {
   addOptimisticTweet: (newTweet: TweetWithAuthor) => void 
 }) => {
   const router = useRouter()
+  
   const handleLikes = async () => {
     const supabase = createClientComponentClient<Database>()
     const { 
@@ -15,24 +17,32 @@ export const Likes = ({ tweet, addOptimisticTweet } : {
     } = await supabase.auth.getUser()
 
     if (user) {
+      startTransition(() => {
+        if (tweet.user_has_liked_tweet) {
+          addOptimisticTweet({
+            ...tweet,
+            likes: tweet.likes - 1,
+            user_has_liked_tweet: false,
+          })
+        } else {
+          addOptimisticTweet({
+            ...tweet,
+            likes: tweet.likes + 1,
+            user_has_liked_tweet: true,
+          })
+        }
+      })
+  
       if (tweet.user_has_liked_tweet) {
-        addOptimisticTweet({
-          ...tweet,
-          likes: tweet.likes - 1,
-          user_has_liked_tweet: !tweet.user_has_liked_tweet,
-        })
-
-        await supabase.from('likes').delete().match({ user_id: user.id, tweet_id: tweet.id })
+        await supabase
+          .from('likes')
+          .delete()
+          .match({ user_id: user.id, tweet_id: tweet.id })
       } else {
-        addOptimisticTweet({
-          ...tweet,
-          likes: tweet.likes + 1,
-          user_has_liked_tweet: !tweet.user_has_liked_tweet,
-        })
-
-        await supabase.from('likes').insert({ user_id: user.id, tweet_id: tweet.id })
+        await supabase
+          .from('likes')
+          .insert({ user_id: user.id, tweet_id: tweet.id })
       }
-      
       router.refresh()
     }
   }
